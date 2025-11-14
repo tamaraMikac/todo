2b036a7c522e0525cb6b747a1a9918d47943dae3
tamaraMikac <mikactamara48@gmail.com>
2025-11-14 01:01:05 +0100

    Urejen TasksView

---
+++ front-end/src/CustomComponents/TasksView.js
@@ -0,0 +1,76 @@
+import { Component } from "react";
+
+class TasksView extends Component {
+  render() {
+    return (
+      <>
+        <nav
+          className="navbar navbar-expand-lg"
+          style={{
+            background: "linear-gradient(to right, #ffbfd4, #ffcfe1)",
+            padding: "12px 25px",
+            position: "sticky",
+            top: 0,
+            zIndex: 10,
+          }}
+        >
+          <div className="container-fluid px-4 justify-content-between align-items-center">
+            <a
+              className="navbar-brand text-back fw-bold"
+              href="#"
+              style={{
+                fontWeight: "700",
+                color: "#a14c6c",
+                fontSize: "1.5rem",
+                letterSpacing: "1px",
+              }}
+            >
+              🌸 To-do{" "}
+            </a>
+            <div className="d-flex gap-2">
+              <a
+                className="btn"
+                href="#"
+                style={{
+                  backgroundColor: "#fffa",
+                  padding: "8px 20px",
+                  border: "none",
+                  borderRadius: "20px",
+                  color: "#a14c6c",
+                  fontWeight: "bold",
+                  boxShadow: "0px 2px 8px rgba(255,182,193,0.6)",
+                }}
+              >
+                {" "}
+                My to-do lists 📃
+              </a>
+            </div>
+          </div>
+        </nav>
+        <div
+          className="min-vh-100 d-flex flex-column align-items-center justify-content-start pt-5"
+          style={{ backgroundColor: "#fff" }}
+        >
+          <div style={{ fontSize: "5rem", marginBottom: "10px" }}>🌸</div>
+          <a
+            className="btn"
+            href="#"
+            style={{
+              backgroundColor: "#fffa",
+              padding: "8px 20px",
+              border: "none",
+              borderRadius: "20px",
+              fontWeight: "bold",
+              boxShadow: "0px 2px 8px rgba(255,182,193,0.6)",
+              color: "#a14c6c",
+            }}
+          >
+            Add new task
+          </a>
+        </div>
+      </>
+    );
+  }
+}
+
+export default TasksView;
