635c735a9053b32dc3bc647955d927a62fc74558
tamaraMikac <mikactamara48@gmail.com>
2025-11-14 23:50:40 +0100

    dodane CRUD funkcije

--- back-end/db/conn.js
+++ back-end/db/conn.js
@@ -18,4 +18,72 @@ if(err){
     release();
 });
 
-module.exports=pool;
+// UPORABNIKI
+
+//PRIDOBI VSE UPORABNIKE
+async function getUsers() {
+const result = await pool.query("SELECT * FROM users ORDER BY id ASC");
+return result.rows;
+}
+
+//PRIDOBI VSE UPORABNIKE BY EMAIL
+async function getUserByEmail(email) {
+const result = await pool.query("SELECT * FROM users WHERE email = $1", [email])
+return result.rows[0]
+}
+
+//USTVARI NOVEGA UPORABNIKA
+async function createUser(username, email, password) {
+    const results = await pool.query("INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING *", [username, email, password]);
+    return results.rows[0];
+}
+
+//PRIDOBI VSE NALOGE
+async function getTask() {
+    const results = await pool.query("SELECT * FROM tasks ORDER BY id ASC");
+    return results.rows;
+    }
+
+//USTVARI NOVO NALOGO
+async function createTask(title, description, due_date, priority, status) {
+    const results = await pool.query("INSERT INTO tasks (title, description, due_date, priority, status) VALUES ($1, $2, $3, $4, $5) RETURNING *",
+        [title, description, due_date, priority, status] 
+    );
+    return results.rows[0];
+}
+
+//IZBRIŠI TASK
+async function deleteTask(id){
+    const results = await pool.query("DELETE FROM tasks WHERE id=$1 RETURNING *", [id]);
+    return results.rows[0];
+}
+
+//PRIDOBI LISTE
+async function getLists() {
+    const results = await pool.query("SELECT * FROM lists ORDER BY id ASC");
+    return results.rows;
+}
+
+//DODAJ NOVO LISTO
+async function createList(title) {
+    const results = await pool.query("INSERT INTO lists (title) VALUES ($1) RETURNING *", [title]);
+    return results.rows[0];
+}
+
+//IZBRIŠI LISTO
+async function deleteList(id) {
+    const results = await pool.query("DELETE FROM lists WHERE id = $1 RETURNING *", [id]);
+    return results.rows[0];
+}
+module.exports={
+    pool,
+    getLists,
+    getTask,
+    getUserByEmail,
+    getUsers,
+    createList,
+    createTask,
+    createUser,
+    deleteList,
+    deleteTask
+};
